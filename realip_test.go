869b589fc23490b374b4597a50a64eed4da1f3e9
Ho Hai Thanh <thanhhh@lifetimetech.vn>
2018-05-27 02:57:54 +0700

    Add RealIP middleware for gin

---
+++ realip_test.go
@@ -0,0 +1,59 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TestXRealIP(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Add("X-Real-IP", "100.100.100.100")
+	w := httptest.NewRecorder()
+
+	r := gin.New()
+	r.Use(RealIP())
+
+	realIP := ""
+	r.GET("/", func(c *gin.Context) {
+		realIP = c.Request.RemoteAddr
+		w.Write([]byte("Hello World"))
+	})
+
+	r.ServeHTTP(w, req)
+
+	if w.Code != 200 {
+		t.Fatal("Response Code should be 200")
+	}
+
+	if realIP != "100.100.100.100" {
+		t.Fatal("Test get real IP error.")
+	}
+
+}
+
+func TestXForwardForIP(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Add("X-Forwarded-For", "100.100.100.100")
+	w := httptest.NewRecorder()
+
+	r := gin.New()
+	r.Use(RealIP())
+
+	realIP := ""
+	r.GET("/", func(c *gin.Context) {
+		realIP = c.Request.RemoteAddr
+		w.Write([]byte("Hello World"))
+	})
+	r.ServeHTTP(w, req)
+
+	if w.Code != 200 {
+		t.Fatal("Response Code should be 200")
+	}
+
+	if realIP != "100.100.100.100" {
+		t.Fatal("Test get real IP error.")
+	}
+}
